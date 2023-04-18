d5d1ac1e6b2ba2cf6d289d454d1860292f260b6e
atlomak <andrzej.tlomak01@gmail.com>
2023-04-18 22:22:00 +0200

    avg/sum grayscale

---
+++ src/main.cpp
@@ -0,0 +1,82 @@
+#include <Arduino.h>
+#include "esp_camera.h"
+
+#define SERIAL_BAUD_RATE 115200
+
+#define THRESHOLD (uint8_t) 120
+
+void setup() {
+  Serial.begin(SERIAL_BAUD_RATE); // Inicjalizacja portu szeregowego
+  Serial.println("ESP32-CAM started");
+  
+  // Inicjalizacja kamery
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = 5;
+  config.pin_d1 = 18;
+  config.pin_d2 = 19;
+  config.pin_d3 = 21;
+  config.pin_d4 = 36;
+  config.pin_d5 = 39;
+  config.pin_d6 = 34;
+  config.pin_d7 = 35;
+  config.pin_xclk = 0;
+  config.pin_pclk = 22;
+  config.pin_vsync = 25;
+  config.pin_href = 23;
+  config.pin_sscb_sda = 26;
+  config.pin_sscb_scl = 27;
+  config.pin_pwdn = 32;
+  config.pin_reset = -1;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_GRAYSCALE;
+  
+  // Konfiguracja rozmiaru obrazu
+  config.frame_size = FRAMESIZE_QVGA;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+  
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+}
+
+void loop() {
+  
+  camera_fb_t * fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("Camera capture failed");
+    return;
+  }
+  
+  long t_piksels = 0;
+
+  for (size_t i = 0; i < fb->len; i++) {
+    if (fb->buf[i] > THRESHOLD) {
+      t_piksels += 1;
+    }
+    else {
+      fb->buf[i] = 0;
+    }
+  }
+
+  uint32_t sum_pixel_value = 0;
+  for (size_t i = 0; i < fb->len; i++) {
+    sum_pixel_value += fb->buf[i];
+  }
+  uint8_t avg_pixel_value = sum_pixel_value / fb->len;
+  
+  // Wyświetl wartość najjaśniejszego piksela
+  Serial.print("Threshold piksels find: ");
+  Serial.println(t_piksels);
+  Serial.print("Avg pixel value: ");
+  Serial.println(avg_pixel_value);
+  // Zwolnij pamięć po zdjęciu
+  esp_camera_fb_return(fb);
+  
+  // Poczekaj 0.5 sekundy przed wykonaniem kolejnego zdjęcia
+  delay(500);
+}
