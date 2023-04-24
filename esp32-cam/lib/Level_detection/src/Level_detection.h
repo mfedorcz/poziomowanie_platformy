6c622fe02a9fe4ea879d13702717989278ca63c1
atlomak <andrzej.tlomak01@gmail.com>
2023-04-24 18:40:07 +0200

    refactoring, moved to lib folder

---
+++ esp32-cam/lib/Level_detection/src/Level_detection.h
@@ -0,0 +1,15 @@
+#ifndef LEVEL_DETECTION_H
+#include <Arduino.h>
+
+typedef struct {
+  uint32_t x = 0;
+  uint32_t y = 0;
+} pixel_position_t;
+
+pixel_position_t get_position(size_t index, size_t width);
+
+pixel_position_t get_spot(uint32_t x, uint32_t y, uint32_t weight);
+
+
+void print_pixel_sector(pixel_position_t position);
+#endif
--- esp32-cam/src/main.cpp
+++ esp32-cam/src/main.cpp
@@ -1,40 +1,10 @@
 #include <Arduino.h>
 #include "esp_camera.h"
+#include <Level_detection.h>
 
 #define SERIAL_BAUD_RATE 115200
 
 #define THRESHOLD (uint8_t) 120
-
-typedef struct {
-  uint32_t x = 0;
-  uint32_t y = 0;
-} pixel_position_t;
-
-pixel_position_t get_position(size_t index, size_t width) {
-  pixel_position_t position;
-  position.x = index % width;
-  position.y = index / width;
-  return position;
-}
-
-pixel_position_t get_spot(uint32_t x, uint32_t y, uint32_t weight) {
-  pixel_position_t position;
-  if (weight == 0){
-    return position;
-  }
-  position.x = x / weight;
-  position.y = y / weight;
-  return position;
-}
-
-void print_pixel_sector(pixel_position_t position) {
-  if (150 < position.x && position.x < 170 && 110 < position.y && position.y < 130){
-    Serial.printf("Centrum, %d %d \n", position.x, position.y);
-  }
-  else {
-    Serial.printf("X: %d, Y: %d \n", position.x, position.y);
-  }
-}
 
 
 void setup() {
