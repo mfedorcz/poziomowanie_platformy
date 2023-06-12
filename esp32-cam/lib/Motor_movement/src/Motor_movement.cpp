5d3ae7e4c70f81f97e12273f8f1d2de7ba1fb0bc
Piotr Budzynski <01161884@pw.edu.pl>
2023-06-12 14:16:03 +0200

    Added decreasing motor movement algorithm

--- esp32-cam/lib/Motor_movement/src/Motor_movement.cpp
+++ esp32-cam/lib/Motor_movement/src/Motor_movement.cpp
@@ -1,3 +1,30 @@
-void Motor_move_up(){
-    
+#include "Motor_movement.h"
+
+void motor_move_up(int stepsPerRevolution, int movementSpeed) {
+    // zmieniamy kierunek obrotu silnika
+	digitalWrite(dirPin, LOW);
+
+	// obracamy szybko
+	for(int x = 0; x < stepsPerRevolution; x++)
+	{
+		digitalWrite(stepPin, HIGH);
+		delayMicroseconds(movementSpeed);
+		digitalWrite(stepPin, LOW);
+		delayMicroseconds(movementSpeed);
+	}
+}
+
+void motor_move_down(int stepsPerRevolution, int movementSpeed) {
+    // ustalamy kierunek obrotu zgodny z kierunkiem wskazówek zegara
+	digitalWrite(dirPin, HIGH);
+
+	// wolno obracamy silnikiem
+	for(int x = 0; x < stepsPerRevolution; x++)
+	{
+		digitalWrite(stepPin, HIGH);
+		delayMicroseconds(movementSpeed);
+		digitalWrite(stepPin, LOW);
+		delayMicroseconds(movementSpeed);
+	}
+	//delay(500); // czekamy sekundę
 }
--- esp32-cam/lib/Motor_movement/src/Motor_movement.h
+++ esp32-cam/lib/Motor_movement/src/Motor_movement.h
@@ -1,6 +1,36 @@
 #ifndef MOTOR_MOVEMENT_H
 #define MOTOR_MOVEMENT_H
 
-void motor_move_up();
+#include <Arduino.h>
+
+#define dirPin 13
+#define stepPin 15
+
+struct CurrentSet {
+    int direction;
+    int stepsPerRevolution;
+
+    CurrentSet(int direction, int stepsPerRevolution) {
+        this->direction = direction;
+        this->stepsPerRevolution = stepsPerRevolution;
+    }
+
+    void setStats(int direction, int stepsPerRevolution) {
+        this->direction = direction;
+        this->stepsPerRevolution = stepsPerRevolution;
+    }
+
+    void setMotorStats(int direction) {
+        if (direction != this->direction)
+        {
+            this->stepsPerRevolution = this->stepsPerRevolution / 2;
+            this->direction = direction;
+        }
+    }
+};
+
+void motor_move_up(int stepsPerRevolution, int movementSpeed);
+
+void motor_move_down(int stepsPerRevolution, int movementSpeed);
 
 #endif
